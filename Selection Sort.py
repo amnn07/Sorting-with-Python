b1cf2f6df7fe632cb52d150f1404fab78e85c0a2
amnn07 <80694576+amnn07@users.noreply.github.com>
2023-10-23 11:50:10 +0530

    Selection Sort.py

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,13 +1,4 @@
-'''def Selection_Sort(lst):
-   for i in range(0,len(lst)):
-      small=i
-      for j in range(i+i,len(lst)):
-         if lst[small]>lst[j]:
-            small=j
-      lst[i],lst[small]=lst[small],lst[i]
-   return lst
-print(Selection_Sort([8,12,3,14,9]))
-'''
+
 def selection(lst):
     for i in range(0,len(lst)):
         small=i
